7ba70988ac299701756533169b0bebac08c66dbe
scwluaHC <shawn.lua@hashicorp.com>
2023-02-10 15:23:15 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,15 +2,13 @@ provider "aws" {
   region = "ap-southeast-1"
 }
 
-data "aws_availability_zones" "available" {}
-
 module "vpc" {
   source  = "terraform-aws-modules/vpc/aws"
   version = "2.77.0"
 
   name                 = "education"
   cidr                 = "10.0.0.0/16"
-  azs                  = data.aws_availability_zones.available.names
+  azs                  = ["ap-southeast-1a", "ap-southeast-1b", "ap-southeast-1c"]
   public_subnets       = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
   enable_dns_hostnames = true
   enable_dns_support   = true
